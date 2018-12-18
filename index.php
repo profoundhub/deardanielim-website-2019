e1812d122182533aa71d706e98a40c5074f33288
Daniel Core <github@profoundideation.com>
2018-12-18 15:26:10 -0500

    update

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
-Hello,
-Testing the Autho
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Join @deardanielim</title>    
+    <meta class="metaTagTop" name="description" content="Would you like to join the @dearDanielim Family &amp; Community?">
+    <meta class="metaTagTop" name="keywords" content="deardanielim, clickfunnels, landing page, family, community">
+    <meta class="metaTagTop" name="author" content="dearDaniel">
+    <meta class="metaTagTop" property="og:image" content="" id="social-image">
+    <meta property="og:title" content="@deardanielim 2019">
+    <meta property="og:description" content="Would you like to join the @dearDanielim Family &amp; Community?">
+    <meta property="og:url" content="https://join.deardanielim.info/">
+    <meta property="og:type" content="website">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
